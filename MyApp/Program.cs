ba0da093754f1c6a2b5ad852377ea3bec4243913
Abregu Braian Marcelo <marceloooabregu015@gmail.com>
2022-05-17 16:03:28 -0300

    Otro commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola Mundo!");
-Console.WriteLine("Hello World!");
+﻿
+// Console.WriteLine("Hola Mundo!");
+// Console.WriteLine("Escriba un texto ");
+// string texto = Console.ReadLine();
+// int numero = 4;
+// Console.WriteLine("Usted escribio " + texto + " y el numero es " + numero);
+
+Console.WriteLine("Indique un numero ");
+string numero1 = Console.ReadLine();
+Console.WriteLine("Indique otro numero ");
+string numero2 = Console.ReadLine();
+int resultado = Convert.ToInt32(numero1) + Convert.ToInt32(numero2);
+Console.WriteLine("El resultado es: " + resultado);
