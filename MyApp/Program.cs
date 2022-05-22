b3e54304ed652f3412ea971d04fa14d842c6361a
Abregu Braian Marcelo <marceloooabregu015@gmail.com>
2022-05-22 17:04:21 -0300

    ejercicio 1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,13 +1,22 @@
-﻿
-// Console.WriteLine("Hola Mundo!");
-// Console.WriteLine("Escriba un texto ");
-// string texto = Console.ReadLine();
-// int numero = 4;
-// Console.WriteLine("Usted escribio " + texto + " y el numero es " + numero);
+﻿// Ejercicio 1 - Invertir
+int numero, resto, invertido = 0;
+Console.WriteLine("Digite un numero para invertir: ");
+numero = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Indique un numero ");
-string numero1 = Console.ReadLine();
-Console.WriteLine("Indique otro numero ");
-string numero2 = Console.ReadLine();
-double resultado = Convert.ToDouble(numero1) + Convert.ToDouble(numero2);
-Console.WriteLine("El resultado es: " + resultado);
+while (numero >= 1)
+{    
+    resto = numero % 10;
+    numero /= 10;
+    if (invertido == 0)
+    {
+        invertido = (resto * 10);
+    } else {
+        if (numero > 0)
+        {
+            invertido = (invertido + resto) * 10;
+        } else {
+            invertido = invertido + resto;
+        }
+    }
+}
+Console.WriteLine("El numero es: " + invertido);
