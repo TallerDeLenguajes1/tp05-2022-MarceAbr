321ac7b7621779aa75594b690ca2a380116d3d89
Abregu Braian Marcelo <marceloooabregu015@gmail.com>
2022-05-22 17:54:33 -0300

    commit ex1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,22 +1,12 @@
 ﻿// Ejercicio 1 - Invertir
-int numero, resto, invertido = 0;
+int numero, ultimoDig, invertido = 0;
 Console.WriteLine("Digite un numero para invertir: ");
 numero = int.Parse(Console.ReadLine());
 
-while (numero >= 1)
+while (numero > 0)
 {    
-    resto = numero % 10;
-    numero /= 10;
-    if (invertido == 0)
-    {
-        invertido = (resto * 10);
-    } else {
-        if (numero > 0)
-        {
-            invertido = (invertido + resto) * 10;
-        } else {
-            invertido = invertido + resto;
-        }
-    }
+    ultimoDig = numero % 10;
+    invertido = invertido * 10 + ultimoDig;
+    numero = numero/10;
 }
 Console.WriteLine("El numero es: " + invertido);
