316435a585a391bdb3cdc57dc790a5c06f6d2615
Abregu Braian Marcelo <marceloooabregu015@gmail.com>
2022-05-22 21:55:09 -0300

    commit CalcV1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,12 +1,33 @@
-﻿// Ejercicio 1 - Invertir
-int numero, ultimoDig, invertido = 0;
-Console.WriteLine("Digite un numero para invertir: ");
-numero = int.Parse(Console.ReadLine());
+﻿//Ejercicio 2 - CalculadoraV1
+int numero1, numero2, resultado = 0, continuar, valor;
 
-while (numero > 0)
-{    
-    ultimoDig = numero % 10;
-    invertido = invertido * 10 + ultimoDig;
-    numero = numero/10;
-}
-Console.WriteLine("El numero es: " + invertido);
+do
+{
+    Console.WriteLine("Indique la operacion que desea realizar: 1-SUMA / 2-RESTA / 3-MULTIPLICACION / 4-DIVISION: ");
+    valor = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ahora indique el primer numero de la operacion: ");
+    numero1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ahora indique el segundo numero de la operacion: ");
+    numero2 = int.Parse(Console.ReadLine());
+
+    switch (valor)
+    {
+        case 1:
+            resultado = numero1 + numero2;
+            break;
+        case 2:
+            resultado = numero1 - numero2;
+            break;
+        case 3:
+            resultado = numero1 * numero2;
+            break;
+        case 4:
+            resultado = numero1 / numero2;
+            break;
+    }
+
+    Console.WriteLine("El resultado de la operacion es: " + resultado);
+    Console.WriteLine("Desea realizar otra operacion? 1-SI / 0-NO");
+    continuar = int.Parse(Console.ReadLine());
+    Console.WriteLine("\n");
+} while (continuar > 0);
