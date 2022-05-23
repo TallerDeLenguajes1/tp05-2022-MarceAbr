08d79715a35a9a343efedb13b4993cd649dfc130
Abregu Braian Marcelo <marceloooabregu015@gmail.com>
2022-05-22 22:15:13 -0300

    cambiando nombres de valores

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,53 +1,53 @@
 ﻿//Ejercicio 2 - Calculadora V2
-int continuar, valor;
-double numero1, numero2, numero3, resutado;
+int continuar2, valor2;
+double numero3, numero4, numero5, resutado2;
 do
 {
     Console.WriteLine("\nIndique la operacion que desea realizar: 1-VALOR ABS / 2-POTENCIA / 3-RAIZ / 4-SENO / 5-COS / 6-PARTE ENTERA: ");
-    valor = int.Parse(Console.ReadLine());
+    valor2 = int.Parse(Console.ReadLine());
     Console.WriteLine("Ahora indique el numero de la operacion: ");
-    numero1 = double.Parse(Console.ReadLine());
+    numero3 = double.Parse(Console.ReadLine());
 
-    switch (valor)
+    switch (valor2)
     {
         case 1:
-            resutado = Math.Abs(numero1);
-            Console.WriteLine("El valor absoluto del numero es: " + resutado);
+            resutado2 = Math.Abs(numero3);
+            Console.WriteLine("El valor absoluto del numero es: " + resutado2);
             break;
         case 2:
-            Console.WriteLine("El cuadrado del numero es: " + Math.Pow(numero1,2));
+            Console.WriteLine("El cuadrado del numero es: " + Math.Pow(numero3,2));
             break;
         case 3:
-            Console.WriteLine("La raiz cuadrada del numero es: " + Math.Sqrt(numero1));
+            Console.WriteLine("La raiz cuadrada del numero es: " + Math.Sqrt(numero3));
             break;
         case 4:
-            Console.WriteLine("El seno del numero es: " + Math.Sin(numero1));
+            Console.WriteLine("El seno del numero es: " + Math.Sin(numero3));
             break;
         case 5:
-            Console.WriteLine("El coseno del numero es: " + Math.Cos(numero1));
+            Console.WriteLine("El coseno del numero es: " + Math.Cos(numero3));
             break;
         case 6:
-            Console.WriteLine("La parte entera del numero es: " + Math.Truncate(numero1));
+            Console.WriteLine("La parte entera del numero es: " + Math.Truncate(numero3));
             break;
     }
     Console.WriteLine("Desea realizar otra operacion? 1-SI / 0-NO");
-    continuar = int.Parse(Console.ReadLine());
-} while (continuar > 0);
+    continuar2 = int.Parse(Console.ReadLine());
+} while (continuar2 > 0);
 
 Console.WriteLine("\nIndique 2 numeros para comprobar el mayor y menor");
 Console.WriteLine("\nIndique el primer numero: ");
-numero2 = double.Parse(Console.ReadLine());
+numero4 = double.Parse(Console.ReadLine());
 Console.WriteLine("Indique el segundo numero: ");
 numero3 = double.Parse(Console.ReadLine());
 
 Console.WriteLine("\nIndique lo que desea: 1-MAYOR / 2-MENOR: ");
-valor = int.Parse(Console.ReadLine());
+valor2 = int.Parse(Console.ReadLine());
 
-if (valor == 1)
+if (valor2 == 1)
 {
-    Console.WriteLine("El mayor entre ambos numeros es: " + max(numero2, numero3));
+    Console.WriteLine("El mayor entre ambos numeros es: " + max(numero4, numero3));
 } else {
-    Console.WriteLine("El mayor entre ambos numeros es: " + min(numero2, numero3));
+    Console.WriteLine("El mayor entre ambos numeros es: " + min(numero4, numero3));
 }
 
 double max(double num1, double num2)
