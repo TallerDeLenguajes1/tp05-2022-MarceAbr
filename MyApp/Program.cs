1c8c467f8c4826009070484b1ef5e4e93ea7e3e1
Abregu Braian Marcelo <marceloooabregu015@gmail.com>
2022-05-30 01:20:27 -0300

    commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,68 +1,52 @@
-﻿//Ejercicio 2 - Calculadora V2
-int continuar2, valor2;
-double numero3, numero4, numero5;
-do
-{
-    Console.WriteLine("\nIndique la operacion que desea realizar: 1-VALOR ABS / 2-POTENCIA / 3-RAIZ / 4-SENO / 5-COS / 6-PARTE ENTERA: ");
-    valor2 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Ahora indique el numero de la operacion: ");
-    numero3 = double.Parse(Console.ReadLine());
+﻿// Ejercicio 4
 
-    switch (valor2)
+string frase, frase2, frase3, fraseConcatenada, subcadena;
+Console.WriteLine("Escriba una frase: ");
+frase = Console.ReadLine();
+
+int longitudCadena = frase.Length;
+Console.WriteLine("La longitud de la cadena es: " + longitudCadena);
+
+Console.WriteLine("\nmEscriba una segunda frase: ");
+frase2 = Console.ReadLine();
+
+fraseConcatenada = frase + " " + frase2;
+Console.WriteLine("\nLa frase concatenada es: ");
+Console.WriteLine(fraseConcatenada);
+
+Console.WriteLine("\nIndique la longitud de la subcadena: ");
+int num = int.Parse(Console.ReadLine());
+subcadena = fraseConcatenada.Substring(0,num);
+Console.WriteLine("La subcadena es: ");
+Console.WriteLine(subcadena);
+
+// foreach (char item in fraseConcatenada)
+// {
+//     Console.WriteLine(item);
+// }
+
+char letra;
+Console.WriteLine("\nIndique una letra para buscar en la cadena: ");
+letra = char.Parse(Console.ReadLine());
+Console.WriteLine("La letra '" + letra + "' se repite " + ocurrencias(fraseConcatenada, letra) + " veces");
+
+Console.WriteLine("\nCadena en mayusculas: " + fraseConcatenada.ToUpper());
+Console.WriteLine("Cadena en minusculas: " + fraseConcatenada.ToLower());
+
+Console.WriteLine("\nIngrese una tercera frase: ");
+frase3 = Console.ReadLine();
+
+// ------------------------ FUNCIONES ------------------------
+
+int ocurrencias(string cadena, char letra)
+{
+    int cantidad = 0;
+    foreach (char item in cadena)
     {
-        case 1:
-            Console.WriteLine("El valor absoluto del numero es: " + Math.Abs(numero3));
-            break;
-        case 2:
-            Console.WriteLine("El cuadrado del numero es: " + Math.Pow(numero3,2));
-            break;
-        case 3:
-            Console.WriteLine("La raiz cuadrada del numero es: " + Math.Sqrt(numero3));
-            break;
-        case 4:
-            Console.WriteLine("El seno del numero es: " + Math.Sin(numero3));
-            break;
-        case 5:
-            Console.WriteLine("El coseno del numero es: " + Math.Cos(numero3));
-            break;
-        case 6:
-            Console.WriteLine("La parte entera del numero es: " + Math.Truncate(numero3));
-            break;
+        if(item == letra)
+        {
+            cantidad++;
+        }
     }
-    Console.WriteLine("Desea realizar otra operacion? 1-SI / 0-NO");
-    continuar2 = int.Parse(Console.ReadLine());
-} while (continuar2 > 0);
-
-Console.WriteLine("\nIndique 2 numeros para comprobar el mayor y menor");
-Console.WriteLine("\nIndique el primer numero: ");
-numero4 = double.Parse(Console.ReadLine());
-Console.WriteLine("Indique el segundo numero: ");
-numero5 = double.Parse(Console.ReadLine());
-
-Console.WriteLine("\nIndique lo que desea: 1-MAYOR / 2-MENOR: ");
-valor2 = int.Parse(Console.ReadLine());
-
-if (valor2 == 1)
-{
-    Console.WriteLine("El mayor entre ambos numeros es: " + max(numero4, numero5));
-} else {
-    Console.WriteLine("El mayor entre ambos numeros es: " + min(numero4, numero5));
-}
-
-double max(double num1, double num2)
-{
-    if(num1>=num2){
-        return num1;
-    } else {
-        return num2;
-    }
-}
-
-double min(double num1, double num2)
-{
-    if(num1<=num2){
-        return num1;
-    } else {
-        return num2;
-    }
+    return cantidad;
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+a) String es un tipo de variable por referencia cuyo valor asignado no puede ser modificado. La unica forma de para cambiar esta variable es creando una nueva cadena de caracteres y hacer que la referencia apunte a la nueva cadena.
+
+b)  \ '- comilla simple
+    \ "- comillas dobles
+    \\ - barra invertida
+    \ 0 - nulo
+    \ a - alerta
+    \ b - retroceso
+    \ n - nueva línea
+    \ r - retorno de carro
+    \ t - pestaña horizontal
+    \ v - pestaña vertical
+
+c) El caracter $ sirve para usar la interpolacion de cadenas y el @ actúa como un identificador textual para indicar que un literal de cadena se debe interpretar literalmente
